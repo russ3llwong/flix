bdb23c3d516b8dac5f978b1863a4e1ade2c848ea
Russell Wong <russell@russells-macbook-air.local>
2019-02-09 22:17:07 -0800

    Flix part 2 - add a tab that leads to a collection view of movies

---
+++ flix/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by Russell Wong on 2/9/19.
+//  Copyright © 2019 Russell Wong. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
